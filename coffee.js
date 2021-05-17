f93dcc5feac05297a54c91eb385e5b80e0e718b5
gyomuitaku <yo0314baseball@gmail.com>
2021-05-17 10:07:02 +0900

    top

--- coffee.js
+++ coffee.js
@@ -1,13 +1,12 @@
 $(function(){
     $('header a').click(function(){
         var id = $(this).attr('href');
-        var position = $(id).offset().top;
+        var position = $(id).offset();
         $('html,body').animate({
-            'scrollTop':position
+            'scrollTop':position.top
         },500);
     });
     $('.header-list h1').click(function(){
-        // $('html,body').scrollTop(0);
         $('html,body').animate({
             'scrollTop':0
         },500)
